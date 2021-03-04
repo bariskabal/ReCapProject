4b77f2cb36755db3e213f98fbd09b9b0bdc0b89e
bariskabal <bariskabal12@gmail.com>
2021-03-04 19:15:37 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EF.Repositories;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager1 = new CarManager(new EFCarRepository());
+            carManager1.Add(new Entities.Concrete.Car()
+            {Id=1, BrandId = 1, ColorId = 1, CarName = "Doblo", ModelYear = "2005", DailyPrice = 50, Description = "Arabaya dair herhangi bir sorundan müşteri sorumludur." });
+        }
+    }
+}
---
+++ DataAccess/Concrete/EF/Repositories/EFColorRepository.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.EF.Context;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EF.Repositories
+{
+    public class EFColorRepository : IColorDal
+    {
+        public void Add(Color entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var addedContext = context.Entry(entity);
+                addedContext.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Color entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var deletedContext = context.Entry(entity);
+                deletedContext.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Color Get(Expression<Func<Color, bool>> filter)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                return context.Set<Color>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                return filter == null
+                    ? context.Set<Color>().ToList()
+                    : context.Set<Color>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Color entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var updatedContext = context.Entry(entity);
+                updatedContext.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
